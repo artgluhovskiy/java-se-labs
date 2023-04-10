242e4a4150c425d5c25cecff5a0667de9e4c55fc
artsiom.hlukhouski <gluhovskiy.art@gmail.com>
2023-04-10 20:25:15 +0200

    Script fix

--- samples-shell/scripts/files/sample_1/script.sh
+++ samples-shell/scripts/files/sample_1/script.sh
@@ -8,20 +8,11 @@ NORMAL='\033[0m'
 echo -e "Scanning $@"
 find "$@" \( -execdir test -e {}/.ignore \; -prune \) -o \( -execdir test -d {}/forbidden \; -prune \) -o -type d \
     -exec bash -c '
-<<<<<<< HEAD
       for i in "$@" ; do
             echo -e "'${HIGHLIGHT}'Found directory: $i'${NORMAL}'"
       done
       echo "Done"
     ' {} +
-=======
-      for x do
-            echo -e "'${HIGHLIGHT}'Found directory: $x'${NORMAL}'"
-            echo "PID: $$"
-      done
-      echo "Done"
-    ' find-sh {} +
->>>>>>> origin/master
 
 
 #https://unix.stackexchange.com/questions/93324/how-does-this-find-command-using-find-exec-sh-c-sh-work
