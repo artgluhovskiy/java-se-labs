e94e4d82815ce87996d3bbf46909b5dbd19f2f50
artsiom.hlukhouski <gluhovskiy.art@gmail.com>
2023-04-10 20:13:10 +0200

    Add simple bash script for searching the files

---
+++ samples-shell/scripts/files/sample_1/script.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Print the directories, which don't contain forbidden subdirectory or .ignore file
+
+HIGHLIGHT='\033[0;31m'
+NORMAL='\033[0m'
+
+echo -e "Scanning $@"
+find "$@" \( -execdir test -e {}/.ignore \; -prune \) -o \( -execdir test -d {}/forbidden \; -prune \) -o -type d \
+    -exec bash -c '
+      for i in "$@" ; do
+            echo -e "'${HIGHLIGHT}'Found directory: $i'${NORMAL}'"
+      done
+      echo "Done"
+    ' {} +
+
+
+#https://unix.stackexchange.com/questions/93324/how-does-this-find-command-using-find-exec-sh-c-sh-work
